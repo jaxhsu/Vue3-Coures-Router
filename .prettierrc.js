7afe4428c45a82510e84b340c0e67ae272989edb
jaxhsu <bluepobo@gmail.com>
2023-04-21 16:45:02 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true, // 行位是否使用分號，默認為true
+  trailingComma: 'es5', // 是否使用尾逗號，有三個可選值"<none|es5|all>"
+  singleQuote: true, // 字符串是否使用單引號，默認為false，使用雙引號
+  printWidth: 100, // 一行的字符數，如果超過會進行換行，默認為80
+  indent: 4, // 縮排，預設2
+  tabWidth: 4, // 一個tab代表幾個空格數
+  useTabs: true, // 啟用tab縮進
+  bracketSpacing: true, // 對象大括號直接是否有空格，默認為true，效果：{ foo: bar }
+};
---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarNav" aria-controls="navbarNav"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="#">Features</a> -->
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="#">Pricing</a> -->
+            <router-link :to="{ name: '新增頁面' }" class="nav-link">NewPage</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
